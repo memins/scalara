d980e2dd1fac87d21f1050415a9f77b6c23c7e3f
Emin Sahin <m.emins@yahoo.com>
2022-09-13 20:10:50 +0200

    Home view and sidebar created.Nav links updated

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+  goHome(){
+    this.router.navigate(['/']);
   }
 
 }
